4acd045ee2daa533eaa7e700813c2bd33fdf535d
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-17 01:04:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(words)
   
   
 
-  array = phrase.split("  ")
+  array = phrase.split
   
-  phrase.each do |i|
+  newphrase.each = []
     new_phrase << phrase.reverse!
     new_phrase
   
